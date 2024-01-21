72679855613f99975440545ce298c3705f6deba3
aadrishmahmood <118641419+aadrish7@users.noreply.github.com>
2024-01-21 11:51:16 +0500

    Done BackEnd

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,19 @@ import mongoose from "mongoose";
 import { PORT, MONGOURI } from "./config.js";
 import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+//Allow all requests from all domains & localhost
+app.use(cors());
+//Allow requests from only specified domain
+ app.use(cors({
+     origin: 'http://localhost:3000',
+     methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
+     allowedHeaders: ['Content-Type']
+ }));
+
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
 });
