a5f26b8ed553a68b805c60a1ac2435427d8af104
varunkasyap <77223408+varunkasyap@users.noreply.github.com>
2024-01-21 12:30:42 +0530

    Update bookModel.js
    Improved book schema and added new features

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -1,9 +1,52 @@
 import mongoose from 'mongoose';
-const bookSchema = mongoose.Schema(
-    {title: { type: String, required: true },
-    author: { type: String, required: true },
-    publishYear: { type: Number, required: true },},
-    { timestamps: true }
-    );
 
-export const Book = mongoose.model('Cat', bookSchema);
+const bookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    images: [
+      {
+        public_id: String,
+        url: String,
+      },
+    ],  // <-- Closing curly brace added here
+  },
+  { timestamps: true }
+);
+
+export const Book = mongoose.model('Book', bookSchema);
